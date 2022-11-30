dd01d99cff798696427061874cf74e5c0f40a7c8
sudhirkotila <sudhir.kotila@gmail.com>
2022-11-30 11:12:59 +0530

    Android Key Store Settings
    - Automatic set Keystore password in project settings, no need to add manually all-time at the build time.

---
+++ Unity Scripts/AndroidKeyStoreSetting.cs
@@ -0,0 +1,15 @@
+﻿#if UNITY_ANDROID && UNITY_EDITOR
+
+using UnityEditor;
+using UnityEngine;
+[InitializeOnLoad]
+public class AndroidKeyStoreSetting
+{
+    static AndroidKeyStoreSetting()
+    {
+        PlayerSettings.keystorePass = "123456";
+        PlayerSettings.keyaliasPass = "123456";
+        PlayerSettings.Android.blitType = AndroidBlitType.Always;
+    }
+}
+#endif
